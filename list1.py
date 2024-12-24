7530e0d38613a91602bdac07b51221020a91b5a4
karthika1125 <malukarthika2001@gmail.com>
2024-12-24 09:04:20 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,60 @@
+# list creating
+list1=["apple","banana","cherry","kiwi"]
+# print(list1)
+
+
+# length
+# print(len(list1))
+# print(list1[2])
+# print(list1[0:4])  #sliceing
+# print(list1[:3])
+# print(list1[2:])
+# print(list1[:])
+ 
+# list1[2]="blackcurrent"
+# print(list1)
+# list1[1:3]="blackcurrent","orange"
+# print(list1)
+# list1.insert(2,"orange")
+# print(list1)
+
+# list1.append("orange")
+# print(list1)
+
+
+# list2=["kiwi","orange"]
+# list1.extend(list2)
+# print(list1)
+
+        #  Remove
+# list1.remove("banana")
+# print(list1)        
+
+# list1.pop(3)
+# print(list1)
+
+# list1.pop()
+# print(list1)
+
+# del list1[0]
+# print(list1)
+
+# del list1
+
+# list1.clear()     # empty list
+# print(list1)
+
+
+fruits=["apple","banan","cherry","kiwi","mango"]
+# newlist=[]
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+#         print(newlist)
+
+# newlist=[x for x in fruits if "a" in x]
+# print(newlist)
+
+list2=[1,2,3]
+list3=list1+list2
+print(list3)
---
+++ tuple.py
@@ -0,0 +1,29 @@
+x=("apple","banan","cherry")
+# y=list(x)
+# y[1]="kiwi"
+# x=tuple(y)
+# print(x)
+
+# thistupple=("apple","banan","cherry")
+# y=list(thistupple)
+# y.append("orange")
+# thistupple=tuple(y)
+# print(thistupple)
+
+
+# y=list(x)
+# y.remove("cherry")
+# x=tuple(y)
+# print(x)
+
+# fruits=("apple","banan","cherry",)
+# mytuple = fruits*2
+# print(mytuple) 
+
+
+# y=list(x)
+# y[1:3]="blackcurrent","kiwi"
+# x=tuple(y)
+# print(x) 
+
+# del x 
