d48fbf5d08b0e9cefd5cb795175cc99c08534122
karthika1125 <malukarthika2001@gmail.com>
2024-12-26 13:23:27 +0530

    Add files via upload

---
+++ anony.py
@@ -0,0 +1,27 @@
+                    #  Anonymus function
+
+
+# a=int(input("enter a number :"))
+# x=lambda a:a+10
+# print(x(a))
+
+                    #  Map
+
+# list1=[1,3,7,8]
+# sqr=list(map(lambda x:x**2,list1))
+# print(sqr)
+
+
+                        #  filter
+
+# list1=[1,2,3,4,5]
+# odd=list(filter(lambda x:x%2!=0,list1))
+# print(odd)
+          
+                        #  reduce
+
+# list1=[1,2,3,4]
+# from functools import reduce
+# product=reduce(lambda x,y:x*y,list1)
+# print(product)
+
