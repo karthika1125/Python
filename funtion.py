3ae696e7ffe36df721bde4bcf712a286f7ae6638
karthika1125 <malukarthika2001@gmail.com>
2024-12-30 12:59:34 +0530

    Add files via upload

---
+++ funtion.py
@@ -0,0 +1,41 @@
+            #  function
+# def myfunction():
+#     print("Hi I am karthika")
+# myfunction()
+
+            #   class and object
+# class Myclass:
+#     x=4
+# p1=Myclass() # create a object 
+# print(p1.x)
+
+
+# class MY_value():
+#     f=int(input("Enter a Value : "))      # using user input
+# s1=MY_value()
+# print(s1.f)    
+
+                    # Constructor 
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+# p1=person("john",36)
+# print(p1.name)
+# print(p1.age)
+    
+
+# class my_value():
+#     x=int(input("Enter a value : "))
+#     f=int(input("Enter a second value : "))
+#     c=x+f
+#     b=x%4
+# s1=my_value()
+
+# print(f"the total sum  {s1.c}")
+# print(f"The total : {s1.b}")
+
+
+
+
+        
---
+++ polymorphism.py
@@ -0,0 +1,60 @@
+                 #   overloding 
+
+# def product(a,b):
+#     p=a*b
+#     print(p)
+
+# def product(a,b,c):
+#     p=a*b*c
+#     print(p)
+# product(4,3,2)    
+
+
+                        # overriding
+# class parent:
+
+#     def mymethod(self):
+#         print("call parent method")
+# class Child(parent):
+#     def mymethod(self):
+#         print("Call child method")
+# c=Child()
+# c.mymethod()           
+                
+                            #  Task
+
+
+
+# class Shape:
+#     def __init__(self):
+#         pass
+
+#     def area(self):
+#         return "Area of shape"
+
+#     def perimeter(self):
+#         return "Perimeter of shape"
+
+
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         super().__init__()
+#         self.radius = radius
+
+#     def area(self):
+#         return pi * (self.radius ** 2)
+
+#     def perimeter(self):
+#         return 2 * pi * self.radius
+
+# pi=3.14
+# shape = Shape()
+# circle = Circle(5)
+
+# print("Shape Area:", shape.area())
+# print("Shape Perimeter:", shape.perimeter())
+# print("Circle Area:", circle.area())
+# print("Circle Perimeter:", circle.perimeter())
+
+
+
