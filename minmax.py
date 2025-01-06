8e11c6bebdd782d7988399da78b7a6bde6a61cff
karthika1125 <malukarthika2001@gmail.com>
2025-01-06 10:15:41 +0530

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,52 @@
+# x=min(5,10,15)
+# y=max(5,10,25)
+# print(x)
+# print(y)
+
+
+
+                #   import math
+# import math
+# x=math.ceil(1.4)
+# y=math.floor(1.6)
+# print(x)
+# print(y)
+
+
+                        #  pi
+# import math
+# x=math.pi
+# print(x)
+
+                        #  datetime
+# import datetime
+# x=datetime.datetime.now()
+# print(x)
+
+# print(x.year)                 #year
+# print(x.strftime("%A"))      #weekday
+# print(x.strftime("%B"))      #Month full version
+# print(x.strftime("%W"))      #week number of year
+# print(x.strftime("%d"))      #day of month
+# print(x.strftime("%Y"))      #year
+# print(x.strftime("%m"))      #Month as number
+# print(x.strftime("%S"))      #Second
+# print(x.strftime("%f"))      #microsecond
+# print(x.strftime("%H"))      #hour 00-23
+# print(x.strftime("%I"))      #hour 00-12
+# print(x.strftime("%p"))      #AM/PM
+# print(x.strftime("%j"))      #day number of year 001-366
+# print(x.strftime("%w"))      #weekay day as number
+# print(x.strftime("%z"))      #UTC offset
+# print(x.strftime("%Z"))      #TimeZone
+# print(x.strftime("%U"))       #weeak number of year
+# print(x.strftime("%x"))        #local version of date
+# print(x.strftime("%X"))        #local version of time
+
+
+                            # random
+# import random
+# random.seed(10)
+# print(random.random())            #Initialize the random number generator
+
+# print(random.getstate())
